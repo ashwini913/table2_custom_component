582a8a2643a72533c0a01a117d2fe7a5e856c9b9
Aswini Chalapathi <aswini.c@decisionpoint.local>
2023-09-13 16:29:08 +0530

    made changes to style , added new features

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="table_update_create_scenario",
-    version="2.1.0",
+    version="2.9.0",
     author="",
     author_email="",
     description="",
